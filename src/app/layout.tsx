3a973a6461645e9c91c0e43f08f6260d138f4926
Daniel Martins <daniellmarttins@hotmail.com>
2023-11-16 11:08:10 -0300

    feat: add global metadata information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,22 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { siteConfig } from "@/config";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Trello Clone",
-  description: "Trello Clone description",
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: [
+    {
+      url: "/images/logo.svg",
+      href: "/images/logo.svg",
+    },
+  ],
 };
 
 export default function RootLayout({
